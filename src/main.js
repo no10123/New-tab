b69e58e97988efebad1adf4bc7110463e0d8ae95
robopugo <reimacdougall@gmail.com>
2026-07-09 12:18:11 -0500

    added clock and random button and a text box.

--- src/main.js
+++ src/main.js
@@ -17,9 +17,10 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response =>
 
     document.querySelector("#app").innerHTML = `
       ${media}
-      <div class="content-overlay">
+      <div class="box">
         <h1 id="title">${data.title}</h1>
         <p id="desc">${data.explanation}</p>
+        <h1 id="clock">00:00:00</h1>
       </div>
     `;
 }).catch(err => {
@@ -57,3 +58,38 @@ datePicker.addEventListener('change', (event) => {
     document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
 });
 });
+
+let Tt = new Date()
+function random(){
+  const date = new Date(Math.random() * Tt.getTime())
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`).then(response => response.json()).then(data => {
+    console.log(data);
+    let media = ""
+    if (data.media_type === "video") {
+      if (data.url.includes("youtube")) {
+        const separator = data.url.includes("?") ? "&" : "?";
+        media = `<iframe id="bg" src="${data.url}${separator}autoplay=1&mute=1&controls=0&loop=1" frameborder="0" allow="autoplay; fullscreen"></iframe>`;
+      } else {
+        media = `<video id="bg" src="${data.url}" autoplay loop muted playsinline></video>`;
+      }
+    } else {
+      media = `<img id="bg" src="${data.url}" alt="${data.title}">`;
+    }
+
+    document.querySelector("#app").innerHTML = `
+      ${media}
+      <div class="content-overlay">
+        <h1 id="title">${data.title}</h1>
+        <p id="desc">${data.explanation}</p>
+      </div>
+    `;
+}).catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
+}
+
+function toggleClock() {
+  document.getElementById('clock').style.opacity = 1 - document.getElementById('clock').style.opacity
+}
+
+setInterval(() => {document.getElementById('clock').textContent = new Date().toLocaleTimeString();}, 1000);
