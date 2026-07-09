9b419b8e5352d68cabee708220e87023d711c68d
robopugo <reimacdougall@gmail.com>
2026-07-09 10:34:38 -0500

    hi

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.createElement("h1")
+document.getElementsByTagName("h1").innerHtml = "hi!"
+
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
