8592cd9d0b946805f542447a4bd1303d9178d963
robopugo <reimacdougall@gmail.com>
2026-07-09 11:07:02 -0500

    added casses for videos and yt links

--- src/main.js
+++ src/main.js
@@ -3,10 +3,23 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
     console.log(data);
+    let t = ""
+    let T = ""
+    if(data.media_type == "video") {
+      t = "video"
+      T = "</video>"
+    } else if (data.url.includes("youtube")) {
+      t = "iframe"
+      T = "</iframe>"
+    } else {
+      t = "img"
+    }
     document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>\n` + 
-    `<img src=${data.url}>\n` + 
+    `<${t} src=${data.url}>${T}\n` + 
     `<p>${data.explanation}</p>`;
-})
+}).catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
 
 
 //const date = document.querySelector("#datepicker").value;
