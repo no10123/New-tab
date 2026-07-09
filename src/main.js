5bee22508118a9cedcfc99963d2cc8d05c49fcb1
robopugo <reimacdougall@gmail.com>
2026-07-09 10:45:40 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
     console.log(data);
+}).then(data => {
+    document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
 })
 
-document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
 
-const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+//const date = document.querySelector("#datepicker").value;
+//fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
