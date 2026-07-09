62cb4e267d04ab1411a7497217d7f92d427587ec
robopugo <reimacdougall@gmail.com>
2026-07-09 10:52:00 -0500

    fix?

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
     console.log(data);
-}).then(data => {
     document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>\n` + 
     `<img src=${data.url}>\n` + 
     `<p>${data.explanation}</p>`;
