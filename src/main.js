84ad5d358ad84fff8fde893cc69f47c64826f985
robopugo <reimacdougall@gmail.com>
2026-07-09 10:48:02 -0500

    add more

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
     console.log(data);
 }).then(data => {
-    document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
+    document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>\n` + 
+    `<img src=${data.url}>\n` + 
+    `<p>${data.explanation}</p>`;
 })
 
 
